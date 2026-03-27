67ed6c0fd3803f7221f11c2e75b7ef857ec9823e
Ivan23BG <ilejeune@outlook.com>
2026-03-27 08:41:38 +0100

    extend features of starter file

--- start.sh
+++ start.sh
@@ -1,10 +1,32 @@
 #!/usr/bin/env bash
-
-# Activate environment
+# Initial setup
+# python3 -m venv .venv
 source .venv/bin/activate
+# pip install -r requirements.txt > /dev/null 2>&1
+# pip freeze > requirements.txt
+# pip install -r requirements.txt > /dev/null 2>&1
 
-# Install dependencies if missing
-pip install -r requirements.txt > /dev/null 2>&1
 
-# Run your project
-python src/main.py
+# Menu system
+case "$1" in
+  python)
+    python Python/main.py
+    ;;
+  test)
+    echo "Running tests..."
+    echo "to fill in"
+    ;;
+  latex)
+    cd LaTeX && python run.py
+    ;;
+  clean)
+    rm -rf __pycache__ .pytest_cache
+    ;;
+  *)
+    echo "Usage:"
+    echo "./start.sh python     ->   run project"
+    echo "./start.sh test    ->   run tests"
+    echo "./start.sh latex   ->   compile LaTeX"
+    echo "./start.sh clean   ->   clean files"
+    ;;
+esac
